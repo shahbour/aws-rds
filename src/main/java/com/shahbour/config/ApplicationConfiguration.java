2908571d28615192343cdac810c86ba6ec0781c8
Ali Shahbour <shahbour@gmail.com>
2017-08-09 15:33:36 +0300

    add import and package

--- src/main/java/com/shahbour/config/ApplicationConfiguration.java
+++ src/main/java/com/shahbour/config/ApplicationConfiguration.java
@@ -1,3 +1,9 @@
+package com.shahbour.config;
+
+
+import org.springframework.cloud.aws.jdbc.config.annotation.EnableRdsInstance;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 @EnableRdsInstance(dbInstanceIdentifier = "portal",password = "Tx9P50LxnPXO", readReplicaSupport = true)
 public class ApplicationConfiguration {
