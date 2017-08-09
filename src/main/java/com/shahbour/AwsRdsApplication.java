c8b075ac6b437d195caab1b72edeb5869df0d3c0
Ali Shahbour <shahbour@gmail.com>
2017-08-09 13:50:26 +0300

    intial commit

---
+++ src/main/java/com/shahbour/AwsRdsApplication.java
@@ -0,0 +1,12 @@
+package com.shahbour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRdsApplication.class, args);
+	}
+}
